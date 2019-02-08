ee0c4487ef715b9c794d20d3c96aca9d4733378a
student14 <student14@student14s-macbook-air.local>
2019-02-07 20:07:29 -0600

    initial Commit

--- Paycheck/ViewController.swift
+++ Paycheck/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //variables
+    var name = "Andy Smith"
+    var employeeId = 54321
+    var regularPay = 0.0
+    var otPay = 0.0
+    var grossPay = 0.0
+    var fedTax = 0.0
+    var stateTax = 0.0
+    var netPay = 0.0
+    
+    @IBOutlet weak var hoursTextField: UITextField!
+    @IBOutlet weak var rateTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    //calculate the net pay based on the hours and rate input
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
